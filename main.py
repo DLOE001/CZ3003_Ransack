3f16dbf3bc0fbcaa3605345def3e17e782aaed42
Nas118 <muhdnasranh@gmail.com>
2021-03-07 19:04:31 +0800

    Second Commit

--- main.py
+++ main.py
@@ -1,94 +1,82 @@
-# EyeLearnSE Game by Team Ransack(2021)
-
-# Brings in all the pygame keywords we need
 from pygame.locals import *
-
-# Main Menu Class
-import mainmenu
-
-# World Select Class
-import worldselect
-
-# Import and initialize the pygame library
-import pygame
+from tiles import *
+from spritesheet import Spritesheet
+from player import Player
+from monster import Monster
+from quiz import Quiz
+################################# LOAD UP A BASIC WINDOW AND CLOCK #################################
 pygame.init()
-
-# Initialise Global Variables
-running = True
-state = 0
-
-# Name of the game
 pygame.display.set_caption("EyeLearnSE")
-
-# Game window resolution
-screen_width = 1200
-screen_height = 900
-
-# Set up the drawing window
-screen = pygame.display.set_mode((screen_width, screen_height))
-  
-# Set the display surface 
-display_surface = pygame.display.set_mode((screen_width, screen_height))
-
-# Colour Coordinates(RGB)
-white = [255, 255, 255]
-
-# Sample User Data
-username = "Daniel Loe"
-user = "Student"
-
-# Simulated Console Login
-# TODO: To be removed once Login page is done
-user = "nil"
-while user == "nil":
-    print("1: Student, 2: Teacher")
-    choice = input()
-    if choice == "1":
-        user = "Student"
-        print("Logged in as Student")
-    elif choice == "2":
-        user = "Teacher"
-        print("Logged in as Teacher")
-    else:
-        print("Input 1 or 2 only!")
-
-# Create Main Menu Object
-menu = mainmenu.MainMenu(username, user, screen, display_surface)
-menu.loadAssets()
-
-#Create World Select Object
-worldselect = worldselect.WorldSelect(username, user, screen, display_surface)
-worldselect.loadAssets()
-
-# Run until the user asks to quit
+DISPLAY_W, DISPLAY_H = 1200, 900
+canvas = pygame.Surface((DISPLAY_W,DISPLAY_H))
+window = pygame.display.set_mode(((DISPLAY_W,DISPLAY_H)))
+running = True
+clock = pygame.time.Clock()
+TARGET_FPS = 60
+################################# LOAD SPRITESHEET PLAYER, MONSTERS AND QUIZ ###################################
+level1spritesheet = Spritesheet('level1spritesheet.png')
+player = Player()
+quiz1 = Quiz("Who is the leader?", "Daniel", "Tom", "Nicklaus", "Alex")
+monster1 = Monster(750, 300, quiz1)
+quiz2 = Quiz("What is the name of our game?", "EyeLearnSE", "Mario", "Box Rush", "Final Fantasy")
+monster2 = Monster(225, 300, quiz2)
+quiz3 = Quiz("What is our team name?", "Ransack", "GroupOne", "EyeLearnSE", "Your mom")
+monster3 = Monster(375, 75, quiz3)
+quiz4 = Quiz("What module is this project?", "CZ3003", "CZ2006", "CZ3002", "CZ2003")
+monster4 = Monster(600, 75, quiz4)
+monsters = []
+monsters.append(monster1)
+monsters.append(monster2)
+monsters.append(monster3)
+monsters.append(monster4)
+#################################### LOAD THE LEVEL #######################################
+background1_image = pygame.image.load('single_background.png')
+map = TileMap('level1.csv', level1spritesheet)
+#monsters = TileMap('monster.csv', monsterspritesheet)
+player.position.x, player.position.y = 0, 800
+################################# GAME LOOP ##########################
 while running:
-
-    # Display the different menus according to state.
-    if state == 0:
-        menu.display()
-    elif state == 1:
-        worldselect.display()
-    elif state == -1:
-        running = False
-    
-    # When user clicks the window close button.
+    dt = clock.tick(60) * .001 * TARGET_FPS
+    ################################# CHECK PLAYER INPUT #################################
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if player.monster == None:
+                if event.key == pygame.K_LEFT:
+                    player.LEFT_KEY = True
+                elif event.key == pygame.K_RIGHT:
+                    player.RIGHT_KEY = True
+                elif event.key == pygame.K_SPACE:
+                    player.jump()
 
-        # When user clicks
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.LEFT_KEY = False
+            elif event.key == pygame.K_RIGHT:
+                player.RIGHT_KEY = False
+            elif event.key == pygame.K_SPACE:
+                if player.is_jumping:
+                    player.velocity.y *= .25
+                    player.is_jumping = False
+
         if event.type == MOUSEBUTTONDOWN:
-            if state == 0:
-                state = menu.action()
-            elif state == 1:
-                state = worldselect.action()
-                    
-        # Draws the surface object to the screen.   
-        pygame.display.update()
+            if player.monster != None:
+                player.monster.quiz.attempt(player)
 
-    # Fills the screen with colour
-    screen.fill(white)
-
+    ################################# UPDATE/ Animate SPRITE #################################
+    player.update(dt, map.tiles, monsters)
+    ################################# UPDATE WINDOW AND DISPLAY #################################
+    canvas.blit(background1_image, [0,0])
+    map.draw_map(canvas)
+    player.draw(canvas)
+    for monster in monsters:
+        monster.draw(canvas)
+    if player.monster != None:
+        player.monster.quiz.draw(window, canvas)
+    window.blit(canvas, (0,0))
+    pygame.display.update()
 
 # Done! Time to quit.
 pygame.quit()
