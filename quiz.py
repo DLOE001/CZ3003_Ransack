37de25da40a22a226e1ac7dffcd5a9681593185b
Nas118 <muhdnasranh@gmail.com>
2021-03-17 00:12:33 +0800

    Made the Quiz answers more readable

--- quiz.py
+++ quiz.py
@@ -27,14 +27,14 @@ class Quiz:
         #Texts
         self.text1 = pygame.font.SysFont('Broadway', 50).render(self.question, True, (0, 0, 0))
         self.text1_position = [120,620]
-        self.text2 = pygame.font.SysFont('Arial', 20).render("A) " + self.answers[0], True, (0, 0, 0))
+        self.text2 = pygame.font.SysFont('Arial', 20, True).render("A) " + self.answers[0], True, (0, 0, 0))
         self.text2_position = self.text2.get_rect().move(120,815)
-        self.text3 = pygame.font.SysFont('Arial', 20).render("B) " + self.answers[1], True, (0, 0, 0))
-        self.text3_position = self.text3.get_rect().move(self.text2_position.right + 100,815)
-        self.text4 = pygame.font.SysFont('Arial', 20).render("C) " + self.answers[2], True, (0, 0, 0))
-        self.text4_position = self.text4.get_rect().move(self.text3_position.right + 100,815)
-        self.text5 = pygame.font.SysFont('Arial', 20).render("D) " + self.answers[3], True, (0, 0, 0))
-        self.text5_position = self.text5.get_rect().move(self.text4_position.right + 100,815)
+        self.text3 = pygame.font.SysFont('Arial', 20, True).render("B) " + self.answers[1], True, (0, 0, 0))
+        self.text3_position = self.text3.get_rect().move(self.text2_position.right + 150,815)
+        self.text4 = pygame.font.SysFont('Arial', 20, True).render("C) " + self.answers[2], True, (0, 0, 0))
+        self.text4_position = self.text4.get_rect().move(self.text3_position.right + 150,815)
+        self.text5 = pygame.font.SysFont('Arial', 20, True).render("D) " + self.answers[3], True, (0, 0, 0))
+        self.text5_position = self.text5.get_rect().move(self.text4_position.right + 150,815)
         #Background
         self.background1_image = pygame.image.load("quiz.png")
         self.background1_position = [0,0]
