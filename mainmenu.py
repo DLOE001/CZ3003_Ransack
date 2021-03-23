7862f64f38d9e2dffed228471ef8d2519274c1f3
perorin2 <72805115+perorin2@users.noreply.github.com>
2021-03-23 17:25:45 +0800

    added logout button to teacher menu, added teacher_logout.png
    other buttons are set to top left corner for now

--- mainmenu.py
+++ mainmenu.py
@@ -81,9 +81,40 @@ class MainMenu:
             self.background1_image = pygame.image.load("images/teacher_mainmenu.jpg")
             self.background1_position = [0,0]
 
+            #Set various main menu button for teacher (needed as actions() checks for these buttons)
+            tempimage = pygame.image.load("images/w2.png")
+            self.button1_image = tempimage
+            self.button2_image = tempimage
+            self.button3_image = tempimage
+            self.button4_image = tempimage
+            self.button5_image = tempimage
+            self.button6_image = tempimage
+
+            self.button1_position = self.button1_image.get_rect().move(0, 0)
+            self.button2_position = self.button2_image.get_rect().move(0, 0)
+            self.button3_position = self.button3_image.get_rect().move(0, 0)
+            self.button4_position = self.button4_image.get_rect().move(0, 0)
+            self.button5_position = self.button5_image.get_rect().move(0, 0)
+            self.button6_position = self.button6_image.get_rect().move(0, 0)
+
             # Set Username Text
             self.teacherUsername = pygame.font.SysFont('Courier New', 40).render(self.username, True, (0, 0, 0))
             self.teacherUsername_position = [835,95]
+
+            #Set logout button for teacher
+            self.logoutbutton7_image = pygame.image.load("images/teacher_logout.png")
+            self.logoutbutton7_position = self.logoutbutton7_image.get_rect().move(715, 168)
+
+            #Set logout popup confirmation for teacher
+            self.logoutpopup_image = pygame.image.load("images/w9.png")
+            self.button9_image = pygame.image.load("images/w10.png")
+            self.button10_image = pygame.image.load("images/w11.png")
+            self.button8_image = pygame.image.load("images/w12.png")
+
+            self.logoutpopup_position = [403,326]
+            self.button8_position = self.button8_image.get_rect().move(812, 332)
+            self.button9_position = self.button9_image.get_rect().move(491, 514)
+            self.button10_position = self.button10_image.get_rect().move(691, 514)
             
     # Display main menu for student and teacher respectively
     def display(self):
@@ -126,11 +157,39 @@ class MainMenu:
             
         #Main Menu Display for Teacher
         if self.user == "Teacher":
-            #Display background for student
+            #Display background for teacher
             self.display_surface.blit(self.background1_image, self.background1_position)
+
+            #Display various main menu button for teacher
+            self.display_surface.blit(self.button1_image, self.button1_position)
+            self.display_surface.blit(self.button2_image, self.button2_position)
+            self.display_surface.blit(self.button3_image, self.button3_position)
+            self.display_surface.blit(self.button4_image, self.button4_position)
+            self.display_surface.blit(self.button5_image, self.button5_position)
+            self.display_surface.blit(self.button6_image, self.button6_position)
+            
+            # Hide above buttons 
+            self.button1_image.set_alpha(0)
+            self.button2_image.set_alpha(0)
+            self.button3_image.set_alpha(0)
+            self.button4_image.set_alpha(0)
+            self.button5_image.set_alpha(0)
+            self.button6_image.set_alpha(0)
      
             # Display Teacher Username
             self.screen.blit(self.teacherUsername, self.teacherUsername_position)
+
+            #Display logout button for student
+            self.display_surface.blit(self.logoutbutton7_image, self.logoutbutton7_position)
+            # Hide logout buttons
+            self.logoutbutton7_image.set_alpha(0)
+
+            #Display logout popup confirmation for student
+            if self.logout:
+                self.display_surface.blit(self.logoutpopup_image, self.logoutpopup_position)
+                self.display_surface.blit(self.button8_image, self.button8_position)
+                self.display_surface.blit(self.button9_image, self.button9_position)
+                self.display_surface.blit(self.button10_image, self.button10_position)
             
     # Main Menu Actions
     def action(self):
